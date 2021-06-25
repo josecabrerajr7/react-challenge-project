999a404cc411d74d6ff308052cb9c9c74e695a6b
Jose <jcdeveloper7@gmail.com>
2021-06-25 07:54:40 -0700

    style: formated redux/store.js

--- application/src/redux/store.js
+++ application/src/redux/store.js
@@ -1,6 +1,6 @@
-import { createStore, applyMiddleware } from 'redux';
-import ReduxThunk from 'redux-thunk';
-import reducers from './reducers';
+import { createStore, applyMiddleware }         from 'redux';
+import ReduxThunk                               from 'redux-thunk';
+import reducers                                 from './reducers';
 
 const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
 
