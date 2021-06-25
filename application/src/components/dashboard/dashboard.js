eb78ff0c2b88fd88167dc9ee2d1f2cf890860ba8
Jose <jcdeveloper7@gmail.com>
2021-06-25 06:38:03 -0700

    style: formated code and removed react-router-dom because it was never used

--- application/src/components/dashboard/dashboard.js
+++ application/src/components/dashboard/dashboard.js
@@ -1,21 +1,20 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import './Dasboard.css';
+import React, { Component } 		from 'react';
+import { connect } 					from 'react-redux';
+import 								'./Dasboard.css';
 
 const mapStateToProps = state => ({
-  temp: state.temp
+	temp: state.temp
 });
 
 class Dashboard extends Component {
-  render() {
-    console.log('props', this.props);
-    return (
-      <div className="main-body">
-        <h1>Dashboard</h1>
-      </div>
-    )
-  }
+	render() {
+		console.log('props from the dashboard', this.props);
+		return (
+			<div className="main-body">
+				<h1>Dashboard</h1>
+			</div>
+		)
+	}
 }
 
 export default connect(mapStateToProps, null)(Dashboard);
