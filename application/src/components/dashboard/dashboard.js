ea7fbc3d8330758603c064ed3021574bf23fc652
Jose <jcdeveloper7@gmail.com>
2021-06-25 06:42:22 -0700

    style: formated code and removed react-router-dom because it was never used

--- application/src/components/dashboard/dashboard.js
+++ application/src/components/dashboard/dashboard.js
@@ -1,6 +1,6 @@
-import React, { Component } 		from 'react';
-import { connect } 					from 'react-redux';
-import 								'./Dasboard.css';
+import React, { Component }			from 'react';
+import { connect }					from 'react-redux';
+import								'./Dasboard.css';
 
 const mapStateToProps = state => ({
 	temp: state.temp
