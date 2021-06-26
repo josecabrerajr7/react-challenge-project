a2af603e307225fd38686b34157256612382e6ee
Jose <jcdeveloper7@gmail.com>
2021-06-25 21:51:21 -0700

    fix: updated my network IPv4

--- application/src/private.js
+++ application/src/private.js
@@ -1,3 +1,3 @@
 const windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'];
 
-export const SERVER_IP = windowsPlatforms.indexOf(window.navigator.platform) < 0 ? 'http://localhost:4000' : 'http://10.0.0.32:4000';
+export const SERVER_IP = windowsPlatforms.indexOf(window.navigator.platform) < 0 ? 'http://localhost:4000' : 'http://192.168.50.208:4000';
