3e0d0bb822b57595a2eef36ec7de028f8c5c6b3a
Jose <jcdeveloper7@gmail.com>
2021-06-26 10:05:44 -0700

    fix(authReducer): changed action.payload.login to action.payload.email. Email was not being return because login parameter.

--- application/src/redux/reducers/authReducer.js
+++ application/src/redux/reducers/authReducer.js
@@ -6,7 +6,7 @@ const INITIAL_STATE             = { email: null, token: null };
  const authReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case LOGIN:
-            return { ...state, email: action.payload.login, token: action.payload.token }
+            return { ...state, email: action.payload.email, token: action.payload.token }
         case LOGOUT:
             return { ...state, ...INITIAL_STATE }
         default:
