5c9e33f0944825d2d8a94ab6d7aad0ba669b8ec9
Jose <jcdeveloper7@gmail.com>
2021-07-03 16:08:14 -0700

    refactor: login to support logout

--- application/src/components/nav/nav.js
+++ application/src/components/nav/nav.js
@@ -1,30 +1,54 @@
-import React            from "react";
-import { Link }         from "react-router-dom";
-import                  "./nav.css";
+import React                        from "react";
+import { connect }                  from "react-redux";
+import { Link, withRouter }         from "react-router-dom";
+import { logoutUser }               from "../../redux/actions/authActions";
+import                              "./nav.css";
 
-const Nav = (props) => {
-    return (
-        <div className="nav-strip">
-            <Link to={"/order"} 
-                  className="nav-link">
+
+
+const mapActionsToProps = dispatch => {
+    return {
+        commenceLogout: () => { dispatch(logoutUser()) }
+    }
+}
+	
+
+const mapStateToProps = state => ({
+	auth: state.auth
+});
+
+class Nav extends React.Component {
+
+
+    logUserOut = () => {
+        this.props.commenceLogout();
+        this.props.history.push('/');
+    }
+    
+    render() {
+        return (
+            <div className="nav-strip">
+                <Link to={"/order"} 
+                      className="nav-link">
+                    <div className="nav-link-style">
+                        <label className="nav-label">Order Form</label>
+                    </div>
+                </Link>
+                <Link to={"/view-orders"} 
+                      className="nav-link" 
+                      id="middle-link">
+                    <div className="nav-link-style">
+                        <label className="nav-label">View Orders</label>
+                    </div>
+                </Link>
                 <div className="nav-link-style">
-                    <label className="nav-label">Order Form</label>
+                    {/* <label className="nav-label" >Log Out</label> */}
+                    <button className="nav-link btn btn-danger" onClick={() => this.logUserOut()}>Log Out</button>
                 </div>
-            </Link>
-            <Link to={"/view-orders"} 
-                  className="nav-link" 
-                  id="middle-link">
-                <div className="nav-link-style">
-                    <label className="nav-label">View Orders</label>
-                </div>
-            </Link>
-            <Link to={"/login"} className="nav-link">
-                <div className="nav-link-style">
-                    <label className="nav-label">Log Out</label>
-                </div>
-            </Link>
-        </div>
-    );
+            </div>
+        );
+    }
+    
 }
 
-export default Nav;
+export default connect(mapStateToProps, mapActionsToProps)(withRouter(Nav));
